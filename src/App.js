1885e5b458c2e7ef2bcdf90a1f500e534e8dbdd7
cristianocsl <cslcristiano@gmail.com>
2021-08-13 16:19:01 -0300

    cria MyContext.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import Cars from './Cars';
+import MyContext from './MyContext';
 
 function App() {
   return (
-    <Cars />
+    <MyContext.Provider value={contextValue}>
+      <Cars />
+    </MyContext.Provider>
   );
 }
 
